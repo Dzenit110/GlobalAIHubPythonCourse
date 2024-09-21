a770e0f6f74c34c0110ed98af8f1559e19777aed
Dzenit Vildic <dzenit6@gmail.com>
2024-09-21 17:42:36 +0200

    Update HW2.py
    Homework 2

--- Homeworks/HW2.py
+++ Homeworks/HW2.py
@@ -1,5 +1,19 @@
-#Explain your work
+from datetime import datetime
 
-#Question 1
-for x in range(a):
-	print(a)
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+birth_year = int(input("Enter your birth year: "))
+
+current_year = datetime.now().year
+age = current_year - birth_year
+
+user_info = [first_name, last_name, age, birth_year]
+
+print("\nUser Information:")
+for info in user_info:
+    print(info)
+
+if age < 18:
+    print("You can't go out because it's too dangerous.")
+else:
+    print("You can go out to the street.")
